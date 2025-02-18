31cf7d87f6d17f19da4194752d18e6eb55e48d70
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-02-18 12:13:24 -0800

    increased timeout otherwise test timeouts

--- ReferenceDeployment/test/int/test.py
+++ ReferenceDeployment/test/int/test.py
@@ -6,5 +6,5 @@ def test_send_command(fprime_test_api):
     Validate that commands are sent and dispatched by sending a NO_OP and ensuring the response events are received
     within half a second.
     """
-    fprime_test_api.send_and_assert_command("ReferenceDeployment.cmdDisp.CMD_NO_OP", max_delay=0.500)
+    fprime_test_api.send_and_assert_command("ReferenceDeployment.cmdDisp.CMD_NO_OP", max_delay=10.000)
     assert fprime_test_api.get_command_test_history().size() == 1
