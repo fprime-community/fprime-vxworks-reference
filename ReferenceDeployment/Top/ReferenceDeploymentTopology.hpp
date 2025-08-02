6efc5c0784ed0dff7853b1182059daad6c738c5b
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-08-01 17:01:43 -0700

    updated comment

--- ReferenceDeployment/Top/ReferenceDeploymentTopology.hpp
+++ ReferenceDeployment/Top/ReferenceDeploymentTopology.hpp
@@ -60,13 +60,7 @@ void setupTopology(const TopologyState& state);
 void teardownTopology(const TopologyState& state);
 
 /**
- * \brief cycle the rate group driver at a crude rate
- *
- * The reference topology does not have a true 1Hz input clock for the rate group driver because it is designed to
- * operate across various computing endpoints (e.g. laptops) where a clear 1Hz source may not be easily and generically
- * achieved. This function mimics the cycling via a Task::delay(milliseconds) loop that manually invokes the ISR call
- * to the example block driver.
- *
+ * \brief start a timer at the specified interval
  *
  * This loop is stopped via a stopRateGroups call.
  *
