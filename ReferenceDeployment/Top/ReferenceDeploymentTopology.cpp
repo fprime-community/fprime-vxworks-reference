ea03785f2c0d01a5150b0159042fefc805a1f766
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-09-26 14:33:38 -0700

    added values to the default arguments. This is a workaround for fprime-vxworks not supporting default values. There is an issue to fix that. For now, add this here

--- ReferenceDeployment/Top/ReferenceDeploymentTopology.cpp
+++ ReferenceDeployment/Top/ReferenceDeploymentTopology.cpp
@@ -76,7 +76,7 @@ void setupTopology(const TopologyState& state) {
     if (state.hostname != nullptr && state.port != 0) {
         Os::TaskString name("ReceiveTask");
         // Uplink is configured for receive so a socket task is started
-        comDriver.start(name, COMM_PRIORITY, Default::STACK_SIZE);
+        comDriver.start(name, COMM_PRIORITY, Default::STACK_SIZE, Os::Task::TASK_DEFAULT, COMM_PRIORITY, Default::STACK_SIZE);
     }
 }
 
